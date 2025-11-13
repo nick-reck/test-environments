8d53619ff55a2eefbaea528145d8c2bde0576587
Nicholas Reck <nick.reck@sogeti.com>
2025-11-13 10:26:19 -0700

    Update cancel script

--- cancel_active_workflow_runs.sh
+++ cancel_active_workflow_runs.sh
@@ -2,10 +2,11 @@
 
 ENV="$1"
 SERVICE="$2"
+CURRENT_RUN_ID="$3"
 REPO="nick-reck/test-environments"
 
-if [[ -z "$ENV" || -z "$SERVICE" ]]; then
-  echo "Usage: $0 <env_to_check> <service>"
+if [[ -z "$ENV" || -z "$SERVICE" || -z "$CURRENT_RUN_ID" ]]; then
+  echo "Usage: $0 <env_to_check> <service> <current_run_id>"
   exit 1
 fi
 
@@ -25,8 +26,28 @@ num_waiting_workflows=${#run_ids[*]}
 echo "$num_waiting_workflows runs with status 'waiting' found."
 echo "Checking pending deployments for each run..."
 
+current_run_created_at=$(gh run view $run_id \
+  --json createdAt \
+  -q '.createdAt')
+# current_run_created_at_timestamp=$(date -juf "%Y-%m-%dT%H:%M:%SZ" "$current_run_created_at" +%s)
+current_run_created_at_timestamp=$(date -d "$current_run_created_at" +"%s")
 job_name_to_cancel="$SERVICE-$ENV"
 for run_id in $run_ids; do
+  pending_run_created_at=$(gh run view $run_id \
+    --json createdAt \
+    -q '.createdAt')
+  if [ -z "$pending_run_created_at" ]; then
+    echo "Failed to retrieve created at date for run $run_id"
+    continue
+  fi
+
+  # pending_run_created_at_timestamp=$(date -juf "%Y-%m-%dT%H:%M:%SZ" "$pending_run_created_at" +%s)
+  pending_run_created_at_timestamp=$(date -d "$pending_run_created_at" +"%s")
+  if (( pending_run_created_at_timestamp > current_run_created_at_timestamp )); then
+    echo "Run $run_id is newer than current run, skipping cancellation"
+    continue
+  fi
+
   pending_job_name=$(gh run view $run_id \
     --json jobs \
     -q '.jobs[] | select(.status == "waiting") | .name')
