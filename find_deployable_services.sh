cf18d4b3ba699e9676ddd8369f3c12b3b6356555
Nicholas Reck <nick.reck@sogeti.com>
2025-11-13 18:41:21 -0700

    Create a new workflow

---
+++ find_deployable_services.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+ENV="$1"
+REPO="nick-reck/test-environments"
+
+if [[ -z "$ENV" ]]; then
+  echo "Usage: $0 <env_to_check_deploys_from>"
+  exit 1
+fi
+
+case "$ENV" in
+    "prod")
+        from_env="test"
+        ;;
+    "test")
+        from_env="dev"
+        ;;
+esac
+
+services=("campaigns" "audience")
+deployable_services=()
+for service in "${services[@]}"; do
+    from_env_deployment_id=$(gh api -XGET \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        /repos/$REPO/deployments \
+        -f 'task='$service \
+        -f 'per_page=1' \
+        -f 'environment='$from_env | jq '.[].id')
+    from_tag=$(gh api \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        /repos/$REPO/deployments/$from_env_deployment_id \
+        | jq -r '.ref')
+    to_env_deployment_id=$(gh api -XGET \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        /repos/$REPO/deployments \
+        -f 'task='$service \
+        -f 'per_page=1' \
+        -f 'environment='$ENV | jq '.[].id')
+    if [[ -z "$to_env_deployment_id" ]]; then
+        deployable_services+=("$from_tag")
+        continue
+    fi
+    to_tag=$(gh api \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        /repos/$REPO/deployments/$to_env_deployment_id \
+        | jq -r '.ref')
+    if [[ "$from_tag" != "$to_tag" ]]; then
+        deployable_services+=("$from_tag")
+        continue
+    fi
+done
+echo "$(jq -c -n '$ARGS.positional' --args "${deployable_services[@]}")"
